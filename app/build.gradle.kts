3f652fe1b464b067eea27ee634d52e29e6046e42
Andrea Bedei <andrea.bedei2@studio.unibo.it>
2024-05-13 23:35:41 +0200

    Fix gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,19 +19,16 @@ repositories {
 }
 
 dependencies {
-    // Use Scala 2.13 in our library project
-    implementation("org.scala-lang:scala-library:2.13.11")
+    implementation("org.scala-lang:scala3-compiler_3:3.4.0")
 
     // This dependency is used by the application.
     implementation("com.google.guava:guava:32.1.1-jre")
 
     // Use Scalatest for testing our library
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.scalatest:scalatest_2.13:3.2.16")
-    testImplementation("org.scalatestplus:junit-4-13_2.13:3.2.2.0")
-
-    // Need scala-xml at test runtime
-    testRuntimeOnly("org.scala-lang.modules:scala-xml_2.13:1.2.0")
+    testImplementation("org.scalatest:scalatest_3:3.2.10")
+    testImplementation("org.scalatestplus:junit-4-13_3:3.2.10.0")
+    testRuntimeOnly("org.scala-lang.modules:scala-xml_3:2.0.1")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
