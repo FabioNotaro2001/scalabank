4bd0fae2891120873cad33183de5813e5d6a47fe
alexmazzotti <alex.mazzotti2@studio.unibo.it>
2024-05-16 17:53:43 +0200

    Add BaseCustomer and its initialization

--- app/src/main/scala/scalabank/entities/Customer.scala
+++ app/src/main/scala/scalabank/entities/Customer.scala
@@ -6,21 +6,21 @@ trait Customer extends Person
 
 trait YoungCustomer extends Customer
 
+trait BaseCustomer extends Customer
 
 object Customer:
   def apply(name: String, surname: String, birthYear: Int): Customer = Person(name, surname, birthYear) match
     case person if person.age < 35 => YoungCustomerImpl(name, surname, birthYear)
-    case _ => ???
+    case _ => BaseCustomerImpl(name, surname, birthYear)
 
-/*
-case class CustomerImpl(_name: String,
-                        _surname: String,
-                        _birthYear: Int) extends Customer:
-  private val person = Person(_name, _surname, _birthYear)
-  export person.*
-*/
 case class YoungCustomerImpl(_name: String,
                              _surname: String,
                              _birthYear: Int) extends YoungCustomer:
+  private val person = Person(_name, _surname, _birthYear)
+  export person.*
+
+case class BaseCustomerImpl(_name: String,
+                             _surname: String,
+                             _birthYear: Int) extends BaseCustomer:
   private val person = Person(_name, _surname, _birthYear)
   export person.*
--- app/src/test/scala/scalabank/entities/CustomerTest.scala
+++ app/src/test/scala/scalabank/entities/CustomerTest.scala
@@ -13,9 +13,15 @@ class CustomerTest extends AnyFunSuite:
     customer.name shouldBe "John"
     customer.surname shouldBe "Doe"
     customer.birthYear shouldBe 1990
+    customer.age shouldBe 34
     customer.isAdult shouldBe true
 
   test("Customer should be a JuniorCustomer if is less 35 years old"):
     val customer = Customer("John", "Doe", 2000)
     assert(customer.isInstanceOf[YoungCustomer])
+    assert(customer.isInstanceOf[Customer])
+
+  test("Customer should be a BaseCustomer if is oldest 35 years old"):
+    val customer = Customer("John", "Doe", 1980)
+    assert(customer.isInstanceOf[BaseCustomer])
     assert(customer.isInstanceOf[Customer])
