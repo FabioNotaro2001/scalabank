fc2ee372f909271585ebdc9801e4bb7e6c01461b
Fabio Notaro <fabio.notaro2@studio.unibo.it>
2024-05-19 22:15:54 +0200

    Implemented the logger with a hidden list

--- app/src/main/scala/scalabank/logger/Logger.scala
+++ app/src/main/scala/scalabank/logger/Logger.scala
@@ -1,4 +1,10 @@
 package scalabank.logger
 
+import scala.collection.mutable.ListBuffer
+
 object Logger:
+    private val savedStrings = ListBuffer[String]()
     def log(string: String): Unit = println(string)
+    def save(string: String): Unit = savedStrings += string
+    def getSize(): Int = savedStrings.size
+    def reset(): Unit = savedStrings.clear()
--- app/src/test/scala/scalabank/testLogger/TestLogger.scala
+++ app/src/test/scala/scalabank/testLogger/TestLogger.scala
@@ -3,14 +3,30 @@ package scalabank.testLogger
 import org.scalatest.matchers.should.Matchers.*
 import scalabank.logger.*
 import org.junit.runner.RunWith
+import org.scalatest.BeforeAndAfterEach
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatestplus.junit.JUnitRunner
 
 
 @RunWith(classOf[JUnitRunner])
-class TestLogger extends AnyFlatSpec:
-  "A logger" should "print" in {
-    Logger.log("ciao")
-  }
+class TestLogger extends AnyFlatSpec with BeforeAndAfterEach:
+  override def beforeEach(): Unit = Logger.reset()
+  
+  "The logger" should "print a string" in:
+    Logger.log("hello")
+
+  "The logger" should "save a string" in :
+    Logger.save("ciao")
+
+  "The logger" should "be empty at the beginning" in:
+    Logger.getSize() shouldBe 0
+    println(Logger.getSize())
+
+  "The logger size" should "grow correctly" in:
+    Logger.save("one")
+    Logger.save("two")
+    Logger.getSize() shouldBe 2
+
+
 
 
