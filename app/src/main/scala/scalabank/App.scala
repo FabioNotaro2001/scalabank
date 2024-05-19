d717faa72570abdacc2b2245c0c136df058d0f29
Fabio Notaro <fabio.notaro2@studio.unibo.it>
2024-05-19 21:47:58 +0200

    Made the logger a singleton

--- app/src/main/scala/scalabank/App.scala
+++ app/src/main/scala/scalabank/App.scala
@@ -7,8 +7,6 @@ import scalabank.logger.Logger
 
 object App {
   def main(args: Array[String]): Unit = {
-    val l: Logger = Logger(true)
-    l.log("ciao")
     println(greeting())
   }
 
--- app/src/main/scala/scalabank/logger/Logger.scala
+++ app/src/main/scala/scalabank/logger/Logger.scala
@@ -1,11 +1,4 @@
 package scalabank.logger
 
-trait Logger:
-  def log(string: String): Unit
-
 object Logger:
-  def apply(on: Boolean): Logger = LoggerImpl(on)
-
-  private case class LoggerImpl(on: Boolean) extends Logger:
-
-    override def log(string: String): Unit = println(string)
+    def log(string: String): Unit = println(string)
