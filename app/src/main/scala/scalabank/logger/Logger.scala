2d7fe7ed5157c910ddbfc5fad7dd843792c7820c
Fabio Notaro <fabio.notaro2@studio.unibo.it>
2024-05-25 14:00:54 +0200

    Changed the list from lazy to buffer into Logger

--- app/src/main/scala/scalabank/logger/Logger.scala
+++ app/src/main/scala/scalabank/logger/Logger.scala
@@ -1,15 +1,17 @@
 package scalabank.logger
 
+import scala.collection.mutable.ListBuffer
+
 object Logger:
     type Event = String
 
-    private var savedEventsList = LazyList[Event]()
+    private var savedEventsList = ListBuffer[Event]()
     private val prefix: String = PrefixFormatter().getStandardPrefixFormatter
 
     def log(event: Event): Unit = println(event)
-    def save(event: Event): Unit = savedEventsList = savedEventsList.prepended(prefix + event)
+    def save(event: Event): Unit = savedEventsList += (prefix + event)
     def getSize(): Int = savedEventsList.size
-    def reset(): Unit = savedEventsList = LazyList[Event]()
+    def reset(): Unit = savedEventsList.clear()
     def logAll(): Unit =
         savedEventsList.foreach(e => Logger.log(e))
         Logger.reset()
