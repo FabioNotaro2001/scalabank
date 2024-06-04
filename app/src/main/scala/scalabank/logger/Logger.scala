8a79e1503fa9a2be1f451c01335a9d95104a3736
Fabio Notaro <fabio.notaro2@studio.unibo.it>
2024-06-05 00:15:05 +0200

    Wrote scaladoc for files related to logger

--- app/src/main/scala/scalabank/logger/Logger.scala
+++ app/src/main/scala/scalabank/logger/Logger.scala
@@ -1,30 +1,111 @@
 package scalabank.logger
 
 import java.io.PrintStream
-import scalabank.logger.PrefixFormatter
 
+/**
+ * Trait defining the Logger functionality.
+ */
 trait Logger:
+    /**
+     * Disables the logger.
+     */
     def disable(): Unit
+
+    /**
+     * Enables the logger.
+     */
     def enable(): Unit
+
+    /**
+     * Checks if the logger is enabled.
+     *
+     * @return true if the logger is enabled, false otherwise.
+     */
     def isEnabledNow: Boolean
+
+    /**
+     * Logs a message.
+     *
+     * @param string the message to be logged.
+     */
     def log(string: String): Unit
+
+    /**
+     * Sets the output media to a file.
+     *
+     * @param fileName the name of the file to which logs will be written.
+     */
     def setOutputMediaToFile(fileName: String): Unit
+
+    /**
+     * Sets the output media to the console.
+     */
     def setOutputMediaToConsole(): Unit
+
+    /**
+     * Gets the prefix formatter.
+     *
+     * @return the current PrefixFormatter instance.
+     */
     def getPrefixFormatter(): PrefixFormatter
 
+/**
+ * Trait for classes that depend on a Logger (cake pattern).
+ */
 trait LoggerDependency:
+    /**
+     * The logger instance.
+     */
     val logger: Logger
 
+/**
+ * Implementation of the Logger trait.
+ */
 class LoggerImpl extends Logger:
     private var isEnabled = true
     private var outputMedia: PrintStream = System.out
     private val prefixFormatter: PrefixFormatter = PrefixFormatter()
 
+    /**
+     * Disables the logger.
+     */
     def disable(): Unit = isEnabled = false
+
+    /**
+     * Enables the logger.
+     */
     def enable(): Unit = isEnabled = true
+
+    /**
+     * Checks if the logger is enabled.
+     *
+     * @return true if the logger is enabled, false otherwise.
+     */
     def isEnabledNow: Boolean = isEnabled
+
+    /**
+     * Logs a message.
+     *
+     * @param string the message to be logged.
+     */
     def log(string: String): Unit =
         if isEnabled then outputMedia.println(prefixFormatter.getPrefixWithCurrentTime + string)
+
+    /**
+     * Sets the output media to a file.
+     *
+     * @param fileName the name of the file to which logs will be written.
+     */
     def setOutputMediaToFile(fileName: String): Unit = outputMedia = PrintStream(fileName + ".txt")
+
+    /**
+     * Sets the output media to the console.
+     */
     def setOutputMediaToConsole(): Unit = outputMedia = System.out
+
+    /**
+     * Gets the prefix formatter.
+     *
+     * @return the current PrefixFormatter instance.
+     */
     def getPrefixFormatter(): PrefixFormatter = prefixFormatter
--- app/src/main/scala/scalabank/logger/PrefixFormatter.scala
+++ app/src/main/scala/scalabank/logger/PrefixFormatter.scala
@@ -2,13 +2,51 @@ package scalabank.logger
 
 import scalabank.utils.TimeFormatter
 
+/**
+ * Trait defining the functionality for formatting log message prefixes.
+ */
 trait PrefixFormatter:
+  /**
+   * Gets the prefix with the current time.
+   *
+   * @return a string representing the prefix with the current time.
+   */
   def getPrefixWithCurrentTime: String
+
+  /**
+   * Gets the creation prefix.
+   *
+   * @return a string representing the creation prefix.
+   */
   def getCreationPrefix: String
 
+/**
+ * Companion object for the PrefixFormatter trait.
+ */
 object PrefixFormatter:
+  /**
+   * Creates a new instance of PrefixFormatter.
+   *
+   * @return a new PrefixFormatter instance.
+   */
   def apply(): PrefixFormatter = PrefixFormatterImpl()
+
+  /**
+   * Private class implementing the PrefixFormatter trait.
+   */
   private class PrefixFormatterImpl extends PrefixFormatter:
     private val timeFormatter = TimeFormatter()
+
+    /**
+     * Gets the prefix with the current time.
+     *
+     * @return a string representing the prefix with the current time.
+     */
     override def getPrefixWithCurrentTime: String = "[" + timeFormatter.getTimeFormatted() + "] "
+
+    /**
+     * Gets the creation prefix.
+     *
+     * @return a string representing the creation prefix.
+     */
     override def getCreationPrefix: String = "[CREATION] "
