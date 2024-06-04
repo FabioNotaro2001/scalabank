0b357c8164a69b9f4053753957f43a7070e1fe3a
Fabio Notaro <fabio.notaro2@studio.unibo.it>
2024-06-05 00:06:27 +0200

    Applied logger and cake pattern to Customer

--- app/src/main/scala/scalabank/entities/Customer.scala
+++ app/src/main/scala/scalabank/entities/Customer.scala
@@ -1,6 +1,8 @@
 package scalabank.entities
 
+import scalabank.entities.Employee.logger
 import scalabank.entities.Person
+import scalabank.logger.{Logger, LoggerDependency, LoggerImpl}
 
 trait Customer extends Person:
   def fidelity: Fidelity
@@ -9,21 +11,32 @@ trait YoungCustomer extends Customer
 
 trait BaseCustomer extends Customer
 
-object Customer:
+trait CustomerComponent:
+  loggerDependency: LoggerDependency =>
+  case class YoungCustomerImpl(_name: String,
+                               _surname: String,
+                               _birthYear: Int) extends YoungCustomer:
+    override def fidelity: Fidelity = Fidelity(0)
+
+    private val person = Person(_name, _surname, _birthYear)
+    export person.*
+
+  case class BaseCustomerImpl(_name: String,
+                              _surname: String,
+                              _birthYear: Int) extends BaseCustomer:
+    override def fidelity: Fidelity = Fidelity(0)
+
+    private val person = Person(_name, _surname, _birthYear)
+    export person.*
+
+object Customer extends LoggerDependency with CustomerComponent:
+  override val logger: Logger = LoggerImpl()
   def apply(name: String, surname: String, birthYear: Int): Customer = Person(name, surname, birthYear) match
-    case person if person.age < 35 => YoungCustomerImpl(name, surname, birthYear)
-    case _ => BaseCustomerImpl(name, surname, birthYear)
-
-case class YoungCustomerImpl(_name: String,
-                             _surname: String,
-                             _birthYear: Int) extends YoungCustomer:
-  override def fidelity: Fidelity = Fidelity(0)
-  private val person = Person(_name, _surname, _birthYear)
-  export person.*
-
-case class BaseCustomerImpl(_name: String,
-                             _surname: String,
-                             _birthYear: Int) extends BaseCustomer:
-  override def fidelity: Fidelity = Fidelity(0)
-  private val person = Person(_name, _surname, _birthYear)
-  export person.*
+      case person if person.age < 35 =>
+        val customer = YoungCustomerImpl(name, surname, birthYear)
+        logger.log(logger.getPrefixFormatter().getCreationPrefix + customer)
+        customer
+      case _ =>
+        val customer = BaseCustomerImpl(name, surname, birthYear)
+        logger.log(logger.getPrefixFormatter().getCreationPrefix + customer)
+        customer
