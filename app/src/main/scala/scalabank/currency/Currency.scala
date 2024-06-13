2d9a65b2d88aaae891eb1b55ff002ad4878773d7
Andrea Bedei <andrea.bedei2@studio.unibo.it>
2024-06-14 00:21:41 +0200

    Create the new class money and test using TDD

--- app/src/main/scala/scalabank/currency/Currency.scala
+++ app/src/main/scala/scalabank/currency/Currency.scala
@@ -1,5 +1,25 @@
 package scalabank.currency
 
-object Currency {
+/**
+ * Represents a currency with code and symbol.
+ */
+trait Currency:
+  /**
+   * The currency code.
+   * @return The code of the currency.
+   */
+  def code: String
 
-}
+  /**
+   * The currency symbol
+   * @return The symbol of the currency.
+   */
+  def symbol: String
+
+
+object Currency:
+  def apply(code: String, symbol: String): Currency = CurrencyImpl(code, symbol)
+
+  private case class CurrencyImpl(override val code: String,
+                                  override val symbol: String) extends Currency
+
--- app/src/test/scala/scalabank/currency/CurrencyTest.scala
+++ app/src/test/scala/scalabank/currency/CurrencyTest.scala
@@ -1,5 +1,46 @@
 package scalabank.currency
 
-object CurrencyTest {
+import org.scalatest.matchers.should.Matchers.*
+import org.junit.runner.RunWith
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatestplus.junit.JUnitRunner
+import scala.concurrent.*
+import scala.concurrent.duration._
 
-}
+@RunWith(classOf[JUnitRunner])
+class CurrencyTest extends AnyFlatSpec:
+  "Currency" should "return the correct code and symbol" in:
+    val currency = Currency("USD", "$")
+    currency.code should be("USD")
+    currency.symbol should be("$")
+
+  it should "create a currency instance with the given code and symbol" in:
+    val currency = Currency("EUR", "€")
+    currency.code should be("EUR")
+    currency.symbol should be("€")
+
+  it should "return the correct exchange rate from a real API call" in:
+    val provider = ExchangeRateProvider()
+    val rateFuture: Future[BigDecimal] = provider.getExchangeRate("USD", "EUR")
+    val rate: BigDecimal = Await.result(rateFuture, 10.seconds)
+    rate should be > BigDecimal(0)
+
+  it should "apply fees correctly" in:
+    val converter = CurrencyConverter("dummyApiKey")
+    val amount = BigDecimal(100)
+    val feeApplied = converter.applyFee(amount, 0.5)
+    feeApplied should be(BigDecimal(99.50))
+
+  "CurrencyConverter" should "convert an amount from one currency to another" in:
+    val converter = CurrencyConverter("dummyApiKey")
+    val fromCurrency = Currency("USD", "$")
+    val toCurrency = Currency("EUR", "€")
+    val amount = BigDecimal(100)
+    val futureResult: Future[BigDecimal] = converter.convert(amount, fromCurrency, toCurrency)
+    val newAmount: BigDecimal = Await.result(futureResult, 5.seconds)
+    assert(newAmount > BigDecimal(0))
+
+
+
+
+
