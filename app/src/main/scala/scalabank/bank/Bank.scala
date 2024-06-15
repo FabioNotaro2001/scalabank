6b5df3deccd46cc5cb52d442f2b994ed82cdb8c1
LeoGiaco <leogiaco2@gmail.com>
2024-06-15 21:26:50 +0200

    Create virtual bank and bank information types

--- app/src/main/scala/scalabank/bank/Bank.scala
+++ app/src/main/scala/scalabank/bank/Bank.scala
@@ -6,13 +6,15 @@ import scalabank.entities.{Customer, Employee}
 import scalabank.logger.{Logger, LoggerDependency, LoggerImpl}
 
 import java.time.LocalDateTime
-import java.util
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 import scala.collection.mutable.{HashMap as MutableHashMap, Map as MutableMap}
 import scala.util.Random
 
+trait BankInformation
+
 trait Bank:
+  def bankInformation: BankInformation
   /**
    * Adds an employee to the bank
    * @param employee the employee to be added
@@ -32,7 +34,7 @@ trait Bank:
    */
   def createAppointment(customer: Customer, description: String, date: LocalDateTime): Appointment
 
-abstract class AbstractBankImpl extends Bank:
+abstract class AbstractBankImpl[T <: BankInformation](override val bankInformation: T) extends Bank:
   protected val employees: ListBuffer[Employee] = ListBuffer()
   protected val customers: ListBuffer[Customer] = ListBuffer()
 
@@ -46,8 +48,11 @@ abstract class AbstractBankImpl extends Bank:
 
 trait BankComponent:
   loggerDependency: LoggerDependency =>
-  case class PhysicalBank(address: String) extends AbstractBankImpl:
+  case class PhysicalBankInformation(name: String, address: String, phoneNumber: String) extends BankInformation
+
+  case class PhysicalBank(override val bankInformation: PhysicalBankInformation) extends AbstractBankImpl[PhysicalBankInformation](bankInformation):
     private val appointments: MutableMap[Customer, ListBuffer[Appointment]] = MutableHashMap()
+
     override def createAppointment(customer: Customer, description: String, date: LocalDateTime): Appointment =
       require(date.isAfter(LocalDateTime.now), "Date of appointment has to be in the future")
       val employee = employees(Random().nextInt(employees.length))
@@ -60,9 +65,13 @@ trait BankComponent:
       customer.addAppointment(appointment)
       employee.addAppointment(appointment)
       appointment
-  
-  // TODO: virtual bank
+
+  case class VirtualBankInformation(name: String, phoneNumber: String) extends BankInformation
+  case class VirtualBank(override val bankInformation: VirtualBankInformation) extends AbstractBankImpl[VirtualBankInformation](bankInformation):
+    override def createAppointment(customer: Customer, description: String, date: LocalDateTime): Appointment =
+      throw UnsupportedOperationException("Virtual banks don't support appointments")
 
 object Bank extends LoggerDependency with BankComponent:
   override val logger: Logger = LoggerImpl()
-  def physicalBank(address: String): Bank = PhysicalBank(address)
+  def physicalBank(name: String, address: String, phoneNumber: String): Bank = PhysicalBank(PhysicalBankInformation(name, address, phoneNumber))
+  def virtualBank(name: String, phoneNumber: String): Bank = VirtualBank(VirtualBankInformation(name, phoneNumber))
--- app/src/test/scala/scalabank/bank/BankTest.scala
+++ app/src/test/scala/scalabank/bank/BankTest.scala
@@ -17,9 +17,18 @@ class BankTest extends AnyFlatSpec with BeforeAndAfterEach:
   private val customer = Customer("Mark", "Baker", 1990)
 
   "A physical bank" should "support creating appointments" in:
-    val bank: Bank = Bank.physicalBank("")
+    val bank: Bank = Bank.physicalBank("Bank", "22 Test St.", "111-2223333")
     bank.addCustomer(customer)
     bank.addEmployee(employee)
     val appointment = bank.createAppointment(customer, "", LocalDateTime.now.plusDays(1))
     customer.getAppointments() should contain (appointment)
     employee.getAppointments should contain (appointment)
+
+  "A virtual bank" should "not have appointments" in:
+    val bank: Bank = Bank.virtualBank("Bank", "111-2223333")
+    bank.addCustomer(customer)
+    bank.addEmployee(employee)
+    assertThrows[UnsupportedOperationException]:
+      bank.createAppointment(customer, "", LocalDateTime.now.plusDays(1))
+
+
