f95018564e371e46e0765622547d69d73d324fc5
Fabio Notaro <fabio.notaro2@studio.unibo.it>
2024-06-15 14:23:12 +0200

    Refactored InterestProvider

--- app/src/main/scala/scalabank/loan/InterestProvider.scala
+++ app/src/main/scala/scalabank/loan/InterestProvider.scala
@@ -5,11 +5,11 @@ import scalabank.loan.*
 trait InterestProvider:
   def getDefaultInterest: InterestRate
   def getInterestForYoungCustomer: InterestRate
-  def getInterestForOldCutsomer: InterestRate
+  def getInterestForOldCustomer: InterestRate
 
 object InterestProvider:
   def apply(): InterestProvider = InterestProviderImpl()
   private class InterestProviderImpl() extends InterestProvider:
     override def getDefaultInterest: InterestRate = InterestRate(0.04)  // TODO: Pensare se questo valore può essere preso tramite API.
     override def getInterestForYoungCustomer: InterestRate = InterestRate(0.03)
-    override def getInterestForOldCutsomer: InterestRate = InterestRate(0.05)
+    override def getInterestForOldCustomer: InterestRate = InterestRate(0.05)
