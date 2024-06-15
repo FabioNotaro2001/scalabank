f499abeb53bfc78f89f2aea9cf725d54ec7d5c97
Fabio Notaro <fabio.notaro2@studio.unibo.it>
2024-06-15 14:07:30 +0200

    Vivided responsibilities between InterestManager and InterestProvider

--- app/src/main/scala/scalabank/loan/InterestManager.scala
+++ app/src/main/scala/scalabank/loan/InterestManager.scala
@@ -1,5 +1,16 @@
 package scalabank.loan
 
-class InterestManager {
+import scalabank.entities.*
+import scalabank.loan.*
 
-}
+trait InterestManager:
+  def findAppropriateInterestForCustomer(customer: Customer): InterestRate
+
+object InterestManager:
+  def apply(): InterestManager = InterestManagerImpl()
+  private class InterestManagerImpl() extends InterestManager:
+    private val interestProvider = InterestProvider()
+    override def findAppropriateInterestForCustomer(customer: Customer): InterestRate = customer match
+      case customer: YoungCustomer => interestProvider.getInterestForYoungCustomer
+      //case customer.isOld => getInterestForOldCustomer  //TODO: far aggiungere a Mazzo isOld.
+      case _ => interestProvider.getDefaultInterest
--- app/src/main/scala/scalabank/loan/InterestProvider.scala
+++ app/src/main/scala/scalabank/loan/InterestProvider.scala
@@ -1,23 +1,15 @@
 package scalabank.loan
 
-import scalabank.entities.*
 import scalabank.loan.*
 
-trait InterestManager:
+trait InterestProvider:
   def getDefaultInterest: InterestRate
   def getInterestForYoungCustomer: InterestRate
   def getInterestForOldCutsomer: InterestRate
-  def findAppropriateInterestForCustomer(customer: Customer): InterestRate
 
-object InterestManager:
-  def apply(): InterestManager = InterestManagerImpl()
-  private class InterestManagerImpl() extends InterestManager:
+object InterestProvider:
+  def apply(): InterestProvider = InterestProviderImpl()
+  private class InterestProviderImpl() extends InterestProvider:
     override def getDefaultInterest: InterestRate = InterestRate(0.04)  // TODO: Pensare se questo valore può essere preso tramite API.
     override def getInterestForYoungCustomer: InterestRate = InterestRate(0.03)
     override def getInterestForOldCutsomer: InterestRate = InterestRate(0.05)
-
-    //TODO: mettere il coso sotto in una classe iversa e separata.
-    override def findAppropriateInterestForCustomer(customer: Customer): InterestRate = customer match
-      case customer: YoungCustomer => getInterestForYoungCustomer
-      //case customer.isOld => getInterestForOldCustomer  //TODO: far aggiungere a Mazzo isOld.
-      case _ => getDefaultInterest
