d377d3fc784da39ddcc0c8f516d9dcb5c256fdfa
Fabio Notaro <fabio.notaro2@studio.unibo.it>
2024-06-17 21:35:53 +0200

    Added logger to LoanCalculator

--- app/src/main/scala/scalabank/loan/LoanCalculator.scala
+++ app/src/main/scala/scalabank/loan/LoanCalculator.scala
@@ -3,15 +3,22 @@ package scalabank.loan
 import scalabank.currency.MoneyADT.Money
 import scalabank.entities.*
 import scalabank.loan.*
+import scalabank.logger.{Logger, LoggerDependency, LoggerImpl}
 
 // TODO: Aggiungere logger a questo file e aggiungere un nuovo prefisso [LOAN_SIMULATION]
 trait LoanCalculator:
   def calculateLoan(customer: Customer, requiredAmount: Money, numberOfPayments: Int): Loan
 
-object LoanCalculator:
-  def apply(): LoanCalculator = LoanCalculatorImpl()
-  private class LoanCalculatorImpl() extends LoanCalculator:
+trait LoanCalculatorComponent:
+  loggerDependency: LoggerDependency =>
+  case class LoanCalculatorImpl() extends LoanCalculator:
     private val interestManager = InterestManager()
     override def calculateLoan(customer: Customer, requiredAmount: Money, numberOfPayments: Int) =
-      assert(numberOfPayments > 0) 
-      Loan(customer, requiredAmount, numberOfPayments, interestManager.findAppropriateInterestForCustomer(customer))
+      assert(numberOfPayments > 0)
+      val loanComputed = Loan(customer, requiredAmount, numberOfPayments, interestManager.findAppropriateInterestForCustomer(customer))
+      loggerDependency.logger.log(logger.getPrefixFormatter().getCreationPrefix + loanComputed)
+      loanComputed
+
+object LoanCalculator extends LoggerDependency with LoanCalculatorComponent:
+  override val logger: Logger = LoggerImpl()
+  def apply(): LoanCalculator = LoanCalculatorImpl()
