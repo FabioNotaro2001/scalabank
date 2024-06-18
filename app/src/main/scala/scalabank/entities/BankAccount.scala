42cff850229a662dc544e9f35cc046f650eec71c
alexmazzotti <alex.mazzotti2@studio.unibo.it>
2024-06-18 18:35:45 +0200

    Add logger

--- app/src/main/scala/scalabank/entities/BankAccount.scala
+++ app/src/main/scala/scalabank/entities/BankAccount.scala
@@ -3,6 +3,7 @@ package scalabank.entities
 import scalabank.currency.MoneyADT.Money
 import scalabank.currency.MoneyADT
 import scalabank.currency.Currency
+import scalabank.logger.{Logger, LoggerDependency, LoggerImpl}
 
 
 enum StateBankAccount:
@@ -16,42 +17,50 @@ trait BankAccount:
     def computeFee: Money
     def computeInterest: Money
 
-case class BaseBankAccountImpl(val _id: Int,
-                      val _balance: Money,
-                      val _currency: Currency,
-                      val _state: StateBankAccount) extends BankAccount:
-
-    override def id: Int = _id
-
-    override def balance: Money = _balance
-
-    override def currency: Currency = _currency
-
-    override def state: StateBankAccount = _state
-
-    override def computeFee: Money = ???
-
-    override def computeInterest: Money = ???
-
-object BaseBankAccount:
+object BaseBankAccount extends LoggerDependency with BankAccountComponent:
+    override val logger: Logger = LoggerImpl()
     def apply(id: Int, balance: Money, currency: Currency, state: StateBankAccount): BaseBankAccountImpl = BaseBankAccountImpl(id, balance, currency, state)
 
-case class SuperBankAccountImpl(val _id: Int,
-                               val _balance: Money,
-                               val _currency: Currency,
-                               val _state: StateBankAccount) extends BankAccount:
-
-    override def id: Int = _id
-
-    override def balance: Money = _balance
-
-    override def currency: Currency = _currency
-
-    override def state: StateBankAccount = _state
-
-    override def computeFee: Money = ???
-
-    override def computeInterest: Money = ???
-
-object SuperBankAccount:
+object SuperBankAccount extends LoggerDependency with BankAccountComponent:
+    override val logger: Logger = LoggerImpl()
     def apply(id: Int, balance: Money, currency: Currency, state: StateBankAccount): SuperBankAccountImpl = SuperBankAccountImpl(id, balance, currency, state)
+
+trait BankAccountComponent:
+    loggerDependency: LoggerDependency =>
+    case class SuperBankAccountImpl(val _id: Int,
+                                   val _balance: Money,
+                                   val _currency: Currency,
+                                   val _state: StateBankAccount) extends BankAccount:
+
+        loggerDependency.logger.log(logger.getPrefixFormatter().getCreationPrefix + this)
+
+        override def id: Int = _id
+
+        override def balance: Money = _balance
+
+        override def currency: Currency = _currency
+
+        override def state: StateBankAccount = _state
+
+        override def computeFee: Money = ???
+
+        override def computeInterest: Money = ???
+
+    case class BaseBankAccountImpl(val _id: Int,
+                                   val _balance: Money,
+                                   val _currency: Currency,
+                                   val _state: StateBankAccount) extends BankAccount:
+
+        loggerDependency.logger.log(logger.getPrefixFormatter().getCreationPrefix + this)
+
+        override def id: Int = _id
+
+        override def balance: Money = _balance
+
+        override def currency: Currency = _currency
+
+        override def state: StateBankAccount = _state
+
+        override def computeFee: Money = ???
+
+        override def computeInterest: Money = ???
