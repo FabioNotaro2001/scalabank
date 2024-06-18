dda25c19b371f6f9543aa811ee50dd15e072c052
Andrea Bedei <andrea.bedei2@studio.unibo.it>
2024-06-18 21:24:52 +0200

    Create the person table and operations to CRUD it

---
+++ app/src/main/scala/scalabank/database/person/PersonTable.scala
@@ -0,0 +1,58 @@
+package scalabank.database.person
+
+import scalabank.database.DatabaseOperations
+import scalabank.entities.Person
+
+import java.sql.{Connection, ResultSet}
+
+class PersonTable(val connection: Connection) extends DatabaseOperations[Person, String]:
+  if !tableExists("person", connection) then
+    val query = "CREATE TABLE IF NOT EXISTS person (cf VARCHAR(16) PRIMARY KEY, name VARCHAR(255), surname VARCHAR(255), birthYear INT)"
+    connection.createStatement().execute(query)
+    populateDB(1)
+
+  def insert(entity: Person): Unit =
+    val stmt = connection.prepareStatement("INSERT INTO person (cf, name, surname, birthYear) VALUES (?, ?, ?, ?)")
+    stmt.setString(1, entity.cf)
+    stmt.setString(2, entity.name)
+    stmt.setString(3, entity.surname)
+    stmt.setInt(4, entity.birthYear)
+    stmt.executeUpdate()
+
+  private def createPerson(resultSet: ResultSet) =
+    Person(resultSet.getString("cf"), resultSet.getString("name"), resultSet.getString("surname"), resultSet.getInt("birthYear"))
+
+  def findById(cf: String): Option[Person] =
+    val stmt = connection.prepareStatement("SELECT * FROM person WHERE cf = ?")
+    stmt.setString(1, cf)
+    val result = stmt.executeQuery()
+    for
+      _ <- Option(result) if result.next()
+    yield createPerson(result)
+
+  def findAll(): Seq[Person] =
+    val stmt = connection.createStatement()
+    val resultSet = stmt.executeQuery("SELECT * FROM person")
+    new Iterator[Person]:
+      def hasNext: Boolean = resultSet.next()
+      def next(): Person = createPerson(resultSet)
+    .toSeq
+
+  def update(entity: Person): Unit =
+    val stmt = connection.prepareStatement("UPDATE person SET name = ?, surname = ?, birthYear = ? WHERE cf = ?")
+    stmt.setString(1, entity.name)
+    stmt.setString(2, entity.surname)
+    stmt.setInt(3, entity.birthYear)
+    stmt.setString(4, entity.cf)
+    stmt.executeUpdate()
+
+  def delete(cf: String): Unit =
+    val stmt = connection.prepareStatement("DELETE FROM person WHERE cf = ?")
+    stmt.setString(1, cf)
+    stmt.executeUpdate()
+
+  private def populateDB(numberOfEntries: Int): Unit =
+      PopulatePersonTable.createInstancesDB(numberOfEntries).foreach(_ => insert(_))
+
+
+
