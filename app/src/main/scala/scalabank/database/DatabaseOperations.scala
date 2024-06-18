7a7eac259c59d793087c051230c80f933b5b9b85
Andrea Bedei <andrea.bedei2@studio.unibo.it>
2024-06-18 21:23:33 +0200

    Update database operations with the check operation

--- app/src/main/scala/scalabank/database/DatabaseOperations.scala
+++ app/src/main/scala/scalabank/database/DatabaseOperations.scala
@@ -1,8 +1,21 @@
 package scalabank.database
 
-trait DatabaseOperations[T]:
+import java.sql.{Connection, ResultSet}
+
+trait DatabaseOperations[T, Q]:
   def insert(entity: T): Unit
-  def findById(id: Long): Option[T]
+  def findById(id: Q): Option[T]
   def findAll(): Seq[T]
   def update(entity: T): Unit
-  def delete(id: Long): Unit
+  def delete(id: Q): Unit
+  def tableExists(tableName: String, connection: Connection): Boolean =
+    val statement = connection.createStatement()
+    try
+      val resultSet: ResultSet = statement.executeQuery(
+        s"SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '$tableName'"
+      )
+      resultSet.next()
+    finally
+      statement.close()
+
+
