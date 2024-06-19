d0e7448ef104544051eed6cdc0c51579f0357260
Andrea Bedei <andrea.bedei2@studio.unibo.it>
2024-06-19 21:44:33 +0200

    fix warning

--- app/src/test/scala/scalabank/entities/AppointmentTest.scala
+++ app/src/test/scala/scalabank/entities/AppointmentTest.scala
@@ -11,7 +11,7 @@ import java.time.LocalDateTime
 @RunWith(classOf[JUnitRunner])
 class AppointmentTest extends AnyFunSuite:
   private val customer = Customer("JHNDOE22B705Y", "John", "Doe", 1990)
-  val employee = Employee("JHNDOE22B705Y", "Alice", "Johnson", 1992, Employee.EmployeePosition.Cashier, 2015)
+  val employee: Employee = Employee("JHNDOE22B705Y", "Alice", "Johnson", 1992, Employee.EmployeePosition.Cashier, 2015)
   private val duration = 30
 
   test("Appointments should have valid parameters"):
