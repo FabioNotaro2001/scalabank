465aab5d19961dde8e87abedcbaa4d5bae885936
alexmazzotti <alex.mazzotti2@studio.unibo.it>
2024-06-19 10:46:51 +0200

    Refactory for specialization of bankAccount

--- app/src/main/scala/scalabank/entities/BankAccount.scala
+++ app/src/main/scala/scalabank/entities/BankAccount.scala
@@ -5,7 +5,6 @@ import scalabank.currency.MoneyADT
 import scalabank.currency.Currency
 import scalabank.logger.{Logger, LoggerDependency, LoggerImpl}
 
-
 enum StateBankAccount:
     case Active, Disactive, Closed
 
@@ -17,50 +16,48 @@ trait BankAccount:
     def computeFee: Money
     def computeInterest: Money
 
+trait BankAccountBehavior extends BankAccount:
+    val _id: Int
+    val _balance: Money
+    val _currency: Currency
+    val _state: StateBankAccount
+    val loggerDependency: LoggerDependency
+
+    loggerDependency.logger.log(loggerDependency.logger.getPrefixFormatter().getPrefixForBankAccountOpening + this)
+
+    override def id: Int = _id
+    override def balance: Money = _balance
+    override def currency: Currency = _currency
+    override def state: StateBankAccount = _state
+
+    override def computeFee: Money = ???
+    override def computeInterest: Money = ???
+
 object BaseBankAccount extends LoggerDependency with BankAccountComponent:
     override val logger: Logger = LoggerImpl()
-    def apply(id: Int, balance: Money, currency: Currency, state: StateBankAccount): BaseBankAccountImpl = BaseBankAccountImpl(id, balance, currency, state)
+    def apply(id: Int, balance: Money, currency: Currency, state: StateBankAccount): BaseBankAccountImpl =
+        BaseBankAccountImpl(id, balance, currency, state, this)
 
 object SuperBankAccount extends LoggerDependency with BankAccountComponent:
     override val logger: Logger = LoggerImpl()
-    def apply(id: Int, balance: Money, currency: Currency, state: StateBankAccount): SuperBankAccountImpl = SuperBankAccountImpl(id, balance, currency, state)
+    def apply(id: Int, balance: Money, currency: Currency, state: StateBankAccount): SuperBankAccountImpl =
+        SuperBankAccountImpl(id, balance, currency, state, this)
 
 trait BankAccountComponent:
     loggerDependency: LoggerDependency =>
-    case class SuperBankAccountImpl(val _id: Int,
-                                   val _balance: Money,
-                                   val _currency: Currency,
-                                   val _state: StateBankAccount) extends BankAccount:
 
-        loggerDependency.logger.log(logger.getPrefixFormatter().getPrefixForBankAccountOpening + this)
+    case class BaseBankAccountImpl(
+                                    _id: Int,
+                                    _balance: Money,
+                                    _currency: Currency,
+                                    _state: StateBankAccount,
+                                    loggerDependency: LoggerDependency
+                                  ) extends BankAccount with BankAccountBehavior
 
-        override def id: Int = _id
-
-        override def balance: Money = _balance
-
-        override def currency: Currency = _currency
-
-        override def state: StateBankAccount = _state
-
-        override def computeFee: Money = ???
-
-        override def computeInterest: Money = ???
-
-    case class BaseBankAccountImpl(val _id: Int,
-                                   val _balance: Money,
-                                   val _currency: Currency,
-                                   val _state: StateBankAccount) extends BankAccount:
-
-        loggerDependency.logger.log(logger.getPrefixFormatter().getPrefixForBankAccountOpening + this)
-
-        override def id: Int = _id
-
-        override def balance: Money = _balance
-
-        override def currency: Currency = _currency
-
-        override def state: StateBankAccount = _state
-
-        override def computeFee: Money = ???
-
-        override def computeInterest: Money = ???
+    case class SuperBankAccountImpl(
+                                     _id: Int,
+                                     _balance: Money,
+                                     _currency: Currency,
+                                     _state: StateBankAccount,
+                                     loggerDependency: LoggerDependency
+                                   ) extends BankAccount with BankAccountBehavior
