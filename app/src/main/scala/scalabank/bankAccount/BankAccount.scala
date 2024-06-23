55a580189bda572a69e350fc157d99de8e68d6b8
Andrea Bedei <andrea.bedei2@studio.unibo.it>
2024-06-23 16:58:07 +0200

    Add new Methods in BankAccount to withdraw and deposit

--- app/src/main/scala/scalabank/bankAccount/BankAccount.scala
+++ app/src/main/scala/scalabank/bankAccount/BankAccount.scala
@@ -1,10 +1,15 @@
-package scalabank.entities
+package scalabank.bankAccount
 
 import scalabank.bank.BankAccountType
+import scalabank.bankAccount.Movement
 import scalabank.currency.MoneyADT.Money
-import scalabank.currency.MoneyADT
-import scalabank.currency.Currency
+import scalabank.currency.{Currency, FeeManager, MoneyADT}
 import scalabank.logger.{Logger, LoggerDependency, LoggerImpl}
+import scalabank.currency.MoneyADT.toMoney
+import scalabank.entities.*
+
+import scala.collection.SeqView
+import scala.collection.immutable.List
 
 enum StateBankAccount:
     case Active, Inactive, Closed
@@ -16,6 +21,10 @@ trait BankAccount:
     def currency: Currency
     def state: StateBankAccount
     def bankAccountType: BankAccountType
+    def movements: SeqView[Movement]
+    def deposit(amount: Money): Unit
+    def withdraw(amount: Money): Unit
+
 
 object BankAccount extends LoggerDependency with BankAccountComponent:
     override val logger: Logger = LoggerImpl()
@@ -28,11 +37,15 @@ trait BankAccountComponent:
     case class BankAccountImpl(
                                 _id: Int,
                                 _customer: Customer,
-                                _balance: Money,
+                                var _balance: Money,
                                 _currency: Currency,
                                 _state: StateBankAccount,
-                                _bankAccountType: BankAccountType
+                                _bankAccountType: BankAccountType,
                               ) extends BankAccount:
+        loggerDependency.logger.log(loggerDependency.logger.getPrefixFormatter().getPrefixForBankAccountOpening + this)
+
+        private var _movements: List[Movement] = List()
+
         override def id: Int = _id
 
         override def customer: Customer = _customer
@@ -45,5 +58,25 @@ trait BankAccountComponent:
 
         override def bankAccountType: BankAccountType = _bankAccountType
 
-        loggerDependency.logger.log(loggerDependency.logger.getPrefixFormatter().getPrefixForBankAccountOpening + this)
+        override def movements: SeqView[Movement] = _movements.view
+
+        override def deposit(amount: Money): Unit =
+            _balance = _balance + amount
+            val deposit = Deposit(amount)
+            _movements = _movements :+ deposit
+            loggerDependency.logger.log(deposit.toString)
+
+        override def withdraw(amount: Money): Unit =
+            val feePerOperation = _bankAccountType.feeXOperation
+            val amountWithFee = FeeManager.calculateAmountWithFee(amount, feePerOperation)
+            require(_balance >= amountWithFee)
+            _balance = _balance - amountWithFee
+            val withdraw = Withdraw(amount, feePerOperation)
+            _movements = _movements :+ withdraw
+            loggerDependency.logger.log(withdraw.toString)
+
+
+
+
+
 
--- app/src/main/scala/scalabank/database/bank/BankAccountTable.scala
+++ app/src/main/scala/scalabank/database/bank/BankAccountTable.scala
@@ -6,6 +6,7 @@ import scalabank.database.DatabaseOperations
 import scalabank.entities.*
 import scalabank.currency.MoneyADT.*
 import scalabank.database.customer.CustomerTable
+import scalabank.bankAccount.{BankAccount, StateBankAccount}
 
 import java.sql.{Connection, ResultSet}
 import scala.util.Random
@@ -42,7 +43,7 @@ class BankAccountTable(val connection: Connection, val customerTable: CustomerTa
     val balance = resultSet.getString("balance")
     val currency = Currency(resultSet.getString("currencyCode"), resultSet.getString("currencySymbol"))
     val state = StateBankAccount.valueOf(resultSet.getString("state"))
-    val accountType = BankAccountType(resultSet.getString("accountType"), BigDecimal(resultSet.getString("fee")))
+    val accountType = BankAccountType(resultSet.getString("accountType"), resultSet.getString("fee").toMoney)
     val customer = customerTable.findById(resultSet.getString("cfOwner")).get
     BankAccount(id, customer, balance.toMoney, currency, state, accountType)
 
@@ -86,9 +87,9 @@ class BankAccountTable(val connection: Connection, val customerTable: CustomerTa
   private def populateDB(): Unit =
     val customers = customerTable.findAll()
     val bankAccountTypes = Seq(
-      BankAccountType("Checking", BigDecimal(0.01)),
-      BankAccountType("Savings", BigDecimal(0.02)),
-      BankAccountType("Business", BigDecimal(0.015))
+      BankAccountType("Checking", 0.01.toMoney),
+      BankAccountType("Savings", 0.02.toMoney),
+      BankAccountType("Business", 0.015.toMoney)
     )
     var idCounter = 1
     val bankAccounts = for
