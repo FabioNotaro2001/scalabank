1565e8b728753fb55230796a6d1f97f64f0ed83c
Andrea Bedei <andrea.bedei2@studio.unibo.it>
2024-06-26 20:13:06 +0200

    Create tests and table for Bank Account Types

---
+++ app/src/test/scala/scalabank/database/BankAccountTypeTest.scala
@@ -0,0 +1,44 @@
+package scalabank.database
+
+import org.scalatest.matchers.should.Matchers.*
+import org.junit.runner.RunWith
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatestplus.junit.JUnitRunner
+import scalabank.currency.MoneyADT.*
+import scalabank.bank.BankAccountType
+import java.sql.{Connection, DriverManager}
+
+@RunWith(classOf[JUnitRunner])
+class BankAccountTypeTest extends AnyFlatSpec:
+  private val database = Database("jdbc:h2:mem:testBankAccountType;DB_CLOSE_DELAY=-1")
+  import database.*
+
+  "The BankAccountTypeTable" should "give the possibility to insert and find a bank account type" in:
+    val bankAccountType = BankAccountType("student", 0.50.toMoney, 0.03)
+    bankAccountTypeTable.insert(bankAccountType)
+    val retrievedBankAccountType = bankAccountTypeTable.findById("student")
+    retrievedBankAccountType should be(defined)
+    retrievedBankAccountType.get shouldEqual bankAccountType
+
+  it should "give the possibility to update a bank account type" in:
+    val updatedBankAccountType = BankAccountType("student", 0.60.toMoney, 0.04)
+    bankAccountTypeTable.update(updatedBankAccountType)
+    val retrievedBankAccountType = bankAccountTypeTable.findById("student")
+    retrievedBankAccountType should be(defined)
+    retrievedBankAccountType.get.feePerOperation shouldEqual 0.60.toMoney
+    retrievedBankAccountType.get.interestSavingJar shouldEqual 0.04
+
+  it should "give the possibility to delete a bank account type" in:
+    bankAccountTypeTable.delete("student")
+    val retrievedBankAccountType = bankAccountTypeTable.findById("student")
+    retrievedBankAccountType should be(empty)
+
+  it should "give the possibility to find all bank account types" in:
+    val bankAccountType1 = BankAccountType("type1", 1.00.toMoney, 0.04)
+    val bankAccountType2 = BankAccountType("type2", 0.75.toMoney, 0.05)
+    bankAccountTypeTable.insert(bankAccountType1)
+    bankAccountTypeTable.insert(bankAccountType2)
+    val allBankAccountTypes = bankAccountTypeTable.findAll()
+    allBankAccountTypes should contain allOf(bankAccountType1, bankAccountType2)
+
+
